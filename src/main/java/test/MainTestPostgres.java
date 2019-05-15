8f546e83066aca07b314352358c8342182a24add
IvanDan <ivandanza@gmail.com>
2019-05-15 22:45:42 +0200

    Create MainTestPostgres.java

---
+++ src/main/java/test/MainTestPostgres.java
@@ -0,0 +1,13 @@
+package prova;
+
+import static java.lang.System.out;
+
+/**
+ * @author IvanDan
+ */
+public class MainTestPostgres {
+
+  public static void main(String[] args) {
+    out.println("Ciao");
+  }
+}
