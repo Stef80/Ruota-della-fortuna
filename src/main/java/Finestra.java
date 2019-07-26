cfb060bb64688ca8b95c2c6e34a2930647e97604
IvanDan <ivandanza@gmail.com>
2019-07-26 15:30:02 +0200

    piccole modifiche

--- src/main/java/Finestra.java
+++ src/main/java/Finestra.java
@@ -1,11 +1,17 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Finestra extends Application {
 
   @Override
-  public void start(Stage primaryStage) {
-
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("finestra.fxml"));
+    primaryStage.setTitle("Finestra");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
