6085fcb3e079c2b0998778ccc773fa04db092031
IvanDan <ivandanza@gmail.com>
2019-08-01 16:15:15 +0200

    creata finestra con i tab

--- src/main/java/Finestra.java
+++ src/main/java/Finestra.java
@@ -8,7 +8,7 @@ public class Finestra extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("finestra.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("finestra_tab.fxml"));
     primaryStage.setTitle("RdF");
     primaryStage.setScene(new Scene(root));
     primaryStage.setResizable(false);
