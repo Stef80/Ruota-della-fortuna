a5a1533cc1d3793a228820dfca8c283b29f9343e
Stefano Longobucco <mide646@gmail.com>
2019-08-03 20:12:35 +0200

    aggiornamento view

--- UML/wellcomeScene/src/sample/Controller.java
+++ UML/wellcomeScene/src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,6 +14,9 @@ import java.awt.*;
 import java.io.IOException;
 
 public class Controller {
+
+	@FXML
+	private Button resetButton;
 
 
 
@@ -36,5 +40,15 @@ public class Controller {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		Main.window.hide();
+	}
+
+	public void reset(ActionEvent event) throws IOException {
+		Parent root1 = FXMLLoader.load(getClass().getResource("/firstPack/forgottenPasswordPane.fxml"));
+		Stage primaryStage = new Stage();
+		Scene scene = new Scene(root1);
+		scene.getStylesheets().add(getClass().getResource("/sample/resources/sampleScene.css").toExternalForm());
+		primaryStage.setTitle("Weel of Fortune");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
