823ca19abf8058b60226f2a72f7c9ae2798c9198
IvanDan <ivandanza@gmail.com>
2019-08-09 09:59:33 +0200

    Update RegistrationManager.java

--- src/main/java/serverRdF/RegistrationManager.java
+++ src/main/java/serverRdF/RegistrationManager.java
@@ -5,9 +5,6 @@ import rdFUtil.logging.User;
 import serverRdF.dbComm.DBManager;
 import serverRdF.dbComm.UsersDTO;
 
-import java.rmi.RemoteException;
-
-
 /**
  * Questa classe gestisce la registrazione dell'utente. I metodi checkEmail e checkNickname permettono di allegerire i controlli sul metodo principale della classe (signUp)
  */
@@ -15,17 +12,16 @@ public class RegistrationManager {
     private DBManager dbManager;
     private static RegistrationManager registrationManager = null;
 
-    private RegistrationManager(DBManager dbManager){
+    private RegistrationManager(DBManager dbManager) {
         this.dbManager = dbManager;
     }
 
 
     /**
-     *
      * @param dbManager il riferimento al manager del db
      * @return registrationManager il singleton della classe.
      */
-    public static RegistrationManager createRegistrationManager(DBManager dbManager){
+    public static RegistrationManager createRegistrationManager(DBManager dbManager) {
         if (registrationManager == null) {
             registrationManager = new RegistrationManager(dbManager);
             return registrationManager;
@@ -34,13 +30,12 @@ public class RegistrationManager {
     }
 
     /**
+     * Registra l'utente nel database. Nel caso non riesca notifica al client l'errore
      *
      * @param form contenente i dati necessari alla registrazione
-     * @param c il riferimento al client
-     *
-     * Registra l'utente nel database. Nel caso non riesca notifica al client l'errore
+     * @param c    il riferimento al client
      */
-    public void signUp(User form, Client c){
+    public void signUp(User form, Client c) {
         boolean bool = dbManager.addUser(form);
         if (!bool) {
             ServerImplementation.serverError(c);
@@ -48,28 +43,24 @@ public class RegistrationManager {
     }
 
     /**
-     *
      * @param email L'indirizzo email da controllare
-     * @return true se l'indirizzo email non è stato già utilizzato, false altrimenti
+     * @return <code>true</code> se l'indirizzo email non è stato già utilizzato, <code>false</code>false altrimenti
      */
-    public boolean checkEmail(String email){
+    public boolean checkEmail(String email) {
         UsersDTO user = dbManager.getUser(true, email);
-        if(user != null){
+        if (user != null) {
             return false;
-        }
-        else return true;
+        } else return true;
     }
 
     /**
-     *
      * @param nickname il nickname da controllare
      * @return true se il nickname non è stato già utilizzato, false altrimenti
      */
-    public boolean checkNickname(String nickname){
+    public boolean checkNickname(String nickname) {
         UsersDTO user = dbManager.getUser(false, nickname);
-        if(user != null){
+        if (user != null) {
             return false;
-        }
-        else return true;
+        } else return true;
     }
 }
