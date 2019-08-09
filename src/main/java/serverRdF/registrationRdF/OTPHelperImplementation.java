54a479aeefde2e8aa66607e528e5ab0a851d2ad2
@Roto <rotoloni.gabriele@gmail.com>
2019-08-09 15:31:14 +0200

    Update OTPHelperImplementation.java

--- src/main/java/serverRdF/registrationRdF/OTPHelperImplementation.java
+++ src/main/java/serverRdF/registrationRdF/OTPHelperImplementation.java
@@ -19,6 +19,14 @@ public class OTPHelperImplementation extends UnicastRemoteObject implements OTPH
         thread = t;
     }
 
+
+    /**
+     * Il client invia il codice per poi ricevere la conferma della registrazione, attraverso {@link WaitingThread}, oppure l'inserimento di un codice errato
+     *
+     * @param otp codice inviato dal client
+     * @param c riferimento al client
+     * @throws RemoteException in caso di errore di connessione al server
+     */
     public void checkOTP(String otp, Client c) throws RemoteException{
         String cryptedOTP = CryptPassword.encrypt(otp);
         if(cryptedOTP.equals(this.otp)){
