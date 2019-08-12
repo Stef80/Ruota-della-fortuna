c3722a6db9605f4cbcfef50c92c0875f186f5e34
IvanDan <ivandanza@gmail.com>
2019-08-12 13:06:32 +0200

    Update DBManagerInterface.java

--- src/main/java/serverRdF/dbComm/DBManagerInterface.java
+++ src/main/java/serverRdF/dbComm/DBManagerInterface.java
@@ -13,7 +13,8 @@ public interface DBManagerInterface {
 
     /**
      * Questo metodo permette di aggiungere una nuova partita nel database
-     * @param id e' l'identificativo della partita
+     *
+     * @param id   e' l'identificativo della partita
      * @param time rappresenta la data e ora di creazione della partita
      * @return <code>true</code> se l'inserimento va a buon fine, altrimenti <code>false</code>
      * @throws SQLException
@@ -35,18 +36,20 @@ public interface DBManagerInterface {
     /**
      * Questo metodo si occupa di cercare all'interno del database cinque frasi che i tre concorrenti non hanno mai
      * visto sia in veste di osservatori che di concorrenti
+     *
      * @param idPlayer1 id del primo concorrente iscritto alla partita
      * @param idPlayer2 id del secondo concorrente iscritto alla partita
      * @param idPlayer3 id del terzo concorrente iscritto alla partita
-     * @return la lista delle cinque frasi, con relativo tema, se trovate, altrimenti null
+     * @return la lista delle cinque frasi, con relativo tema, se trovate, altrimenti <code>null</code>
      * @throws SQLException
      */
     List<PhrasesDTO> get5Phrases(String idPlayer1, String idPlayer2, String idPlayer3) throws SQLException;
 
     /**
      * Questo metodo permette di aggiungere un insieme di frasi e temi all'interno del database
+     *
      * @param phrases lista delle frasi che si vogliono inserire nella base di dati
-     * @param themes lista dei temi, relativi alle frasi, che si vogliono inserire nella base di dati
+     * @param themes  lista dei temi, relativi alle frasi, che si vogliono inserire nella base di dati
      * @return <code>true</code> se l'inserimento va a buon fine, altrimenti <code>false</code>
      * @throws SQLException
      */
@@ -54,7 +57,8 @@ public interface DBManagerInterface {
 
     /**
      * Questo metodo fornisce la lista delle frasi e temi presenti nella base di dati
-     * @return lista delle frasi, e relativi temi, oppure in caso di errori null
+     *
+     * @return lista delle frasi, e relativi temi, oppure in caso di errori <code>null</code>
      * @throws SQLException
      */
     List<PhrasesDTO> getAllPhrases() throws SQLException;
