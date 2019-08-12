7d57a2ace4c6f6773b4691e48783a190a8dee522
IvanDan <ivandanza@gmail.com>
2019-08-12 13:06:52 +0200

    Update OTPHelper.java

--- src/main/java/serverRdF/registrationRdF/OTPHelper.java
+++ src/main/java/serverRdF/registrationRdF/OTPHelper.java
@@ -7,5 +7,12 @@ import java.rmi.RemoteException;
 
 public interface OTPHelper extends Remote {
 
+    /**
+     * Il client invia il codice per poi ricevere la conferma della registrazione, attraverso {@link WaitingThread}, oppure l'inserimento di un codice errato
+     *
+     * @param otp codice inviato dal client
+     * @param c   riferimento al client
+     * @throws RemoteException in caso di errore di connessione al server
+     */
     public void checkOTP(String otp, Client c) throws RemoteException;
 }
