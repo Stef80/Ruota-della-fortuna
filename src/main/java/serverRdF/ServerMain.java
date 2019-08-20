0dfa231b22928e0df5fddf6698bcc6f999d61a02
@Roto <rotoloni.gabriele@gmail.com>
2019-08-20 17:01:37 +0200

    Create ServerMain.java

---
+++ src/main/java/serverRdF/ServerMain.java
@@ -0,0 +1,9 @@
+package serverRdF;
+
+import java.net.InetAddress;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        InetAddress addr = new InetAddress.getByName(null);
+    }
+}
