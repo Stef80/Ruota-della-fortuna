626d04ea89c750e2b5d64cf71ed41144919df08e
IvanDan <ivandanza@gmail.com>
2019-08-20 16:34:38 +0200

    Update ServerImplementation.java

--- src/main/java/serverRdF/ServerImplementation.java
+++ src/main/java/serverRdF/ServerImplementation.java
@@ -10,27 +10,27 @@ import serverRdF.registrationRdF.OTPHelper;
 import java.io.File;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ServerImplementation extends UnicastRemoteObject implements Server{
+public class ServerImplementation extends UnicastRemoteObject implements Server {
 
-    public ServerImplementation() throws RemoteException{}
+    public ServerImplementation() throws RemoteException {
+    }
 
     //TODO campi per i vari managers e implementazione metodi dell'interfaccia. estende UnicastRemoteObject e implementa Server
 
 
     /**
+     * Questo metodo si occupa di gestire la notifica al client nel caso di errori con la connessione al server o al database.
+     * <p>
+     * Viene richiamato in caso di eccezioni come RemoteException o SQLException
      *
      * @param c riferimento al client
-     *
-     * Questo metodo si occupa di gestire la notifica al client nel caso di errori con la connessione al server o al database.
-     * Viene richiamato in caso di eccezioni come RemoteException o SQLException
      */
-    public static void serverError(Client c){
-        if(c == null){
+    public static void serverError(Client c) {
+        if (c == null) {
             System.out.println("Server error");
-        }else {
+        } else {
             try {
                 c.notifyServerError();
             } catch (RemoteException ex) {
@@ -45,7 +45,7 @@ public class ServerImplementation extends UnicastRemoteObject implements Server{
     }
 
     @Override
-    public boolean checkNickName(String name) throws RemoteException {
+    public boolean checkNickname(String nickname) throws RemoteException {
         return false;
     }
 
@@ -56,7 +56,6 @@ public class ServerImplementation extends UnicastRemoteObject implements Server{
 
     @Override
     public void signIn(Login form, Client c) throws RemoteException {
-
     }
 
     @Override
@@ -81,51 +80,41 @@ public class ServerImplementation extends UnicastRemoteObject implements Server{
 
     @Override
     public void addPhrases(File file, Client c) throws RemoteException {
-
     }
 
     @Override
     public void changeName(String name, Client c) throws RemoteException {
-
     }
 
     @Override
     public void changeSurname(String surname, Client c) throws RemoteException {
-
     }
 
     @Override
     public void changeNickname(String nickname, Client c) throws RemoteException {
-
     }
 
     @Override
     public void changePassword(String password, Client c) throws RemoteException {
-
     }
 
     @Override
     public void checkPlayer(Client c) throws RemoteException {
-
     }
 
     @Override
     public void checkPerPlayer(String nickname, Client c) throws RemoteException {
-
     }
 
     @Override
     public void bestMove(Client c) throws RemoteException {
-
     }
 
     @Override
     public void averageManches(Client c) throws RemoteException {
-
     }
 
     @Override
     public void resetPassword(Client c) throws RemoteException {
-
     }
 }
