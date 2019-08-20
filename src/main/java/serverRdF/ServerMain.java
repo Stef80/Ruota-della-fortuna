1315175666f61cf14906fc563be7c799c2078ed3
@Roto <rotoloni.gabriele@gmail.com>
2019-08-20 18:36:57 +0200

    Update ServerMain.java

--- src/main/java/serverRdF/ServerMain.java
+++ src/main/java/serverRdF/ServerMain.java
@@ -1,26 +1,41 @@
 package serverRdF;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class ServerMain {
+    public static final String HOSTNAME = "ServerRdF";
+    public static final int PORT = 1099;
+
     public static void main(String[] args) {
+        /*
         try {
-            InetAddress addr = InetAddress.getLocalHost();
-            String hostName = addr.getHostName();
-            System.out.println(hostName);
+            InetSocketAddress address = new InetSocketAddress(HOSTNAME,PORT);
+            System.out.println(address.getHostName() + address.getAddress().toString());
             Registry r;
-            if(LocateRegistry.getRegistry(1099) == null){
-                r = LocateRegistry.createRegistry(1099);
+            if(LocateRegistry.getRegistry(HOSTNAME,PORT) == null){
+                r = LocateRegistry.createRegistry(PORT);
             }else{
-                r = LocateRegistry.getRegistry(1099);
+                r = LocateRegistry.getRegistry(HOSTNAME,PORT);
             }
             //TODO
-        } catch (UnknownHostException | RemoteException e) {
+        } catch (RemoteException e) {
             ServerImplementation.serverError(null);
+        }
+         */
+        try {
+            InetAddress addr = Inet4Address.getLocalHost();
+            byte[] bb = addr.getAddress();
+            for(Byte b : bb){
+                System.out.print(b);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         }
     }
 }
