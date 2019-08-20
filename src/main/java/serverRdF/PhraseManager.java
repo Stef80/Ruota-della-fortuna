1b3701d31a152185e3a0da01e789d736fe1de806
@Roto <rotoloni.gabriele@gmail.com>
2019-08-20 15:23:56 +0200

    Update PhraseManager.java

--- src/main/java/serverRdF/PhraseManager.java
+++ src/main/java/serverRdF/PhraseManager.java
@@ -1,6 +1,25 @@
 package serverRdF;
 
-public class PhraseManager {
+import serverRdF.dbComm.DBManager;
+import serverRdF.emailRdF.EmailManager;
+import serverRdF.registrationRdF.RegistrationManager;
 
-    //TODO singleton-metodi
+public class PhraseManager {
+    private DBManager dbManager;
+    private static PhraseManager phraseManager = null;
+
+    private PhraseManager(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    public static PhraseManager createPhraseManager(DBManager dbmng){
+        if(phraseManager == null){
+            phraseManager = new PhraseManager(dbmng);
+            return phraseManager;
+        }else{
+            return phraseManager;
+        }
+    }
+
+    //TODO metodi
 }
