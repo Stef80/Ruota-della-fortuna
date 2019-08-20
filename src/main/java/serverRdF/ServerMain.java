d3220076e19b7dee4a23da07f708526ddb72c74c
@Roto <rotoloni.gabriele@gmail.com>
2019-08-20 17:08:11 +0200

    Update ServerMain.java

--- src/main/java/serverRdF/ServerMain.java
+++ src/main/java/serverRdF/ServerMain.java
@@ -1,9 +1,26 @@
 package serverRdF;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class ServerMain {
     public static void main(String[] args) {
-        InetAddress addr = new InetAddress.getByName(null);
+        try {
+            InetAddress addr = InetAddress.getByName(null);
+            String hostName = addr.getHostName();
+            System.out.println(hostName);
+            Registry r;
+            if(LocateRegistry.getRegistry(1099) == null){
+                r = LocateRegistry.createRegistry(1099);
+            }else{
+                r = LocateRegistry.getRegistry(1099);
+            }
+            //TODO
+        } catch (UnknownHostException | RemoteException e) {
+            ServerImplementation.serverError(null);
+        }
     }
 }
