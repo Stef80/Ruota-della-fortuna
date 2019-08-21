b348c7d2c9fadacf959537d53331fa3e9a1dea9a
IvanDan <ivandanza@gmail.com>
2019-08-21 12:34:31 +0200

    recuperato GamePlayerController

--- src/main/java/rdFUtil/view/GamePlayerController.java
+++ src/main/java/rdFUtil/view/GamePlayerController.java
@@ -2,16 +2,34 @@ package rdFUtil.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class GamePlayerController {
-	@FXML
-	private Button jollyButton;
-	@FXML
-	private Button vowelButton;
-	@FXML
-	private Button turnButton;
-	@FXML
-	private Button solutionButton;
-	@FXML
-	private Button exitButton;
+    @FXML
+    private Button jollyButton;
+    @FXML
+    private Button vowelButton;
+    @FXML
+    private Button turnButton;
+    @FXML
+    private Button solutionButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private TextField solutionTextField;
+    @FXML
+    private TextField letterTextField;
+
+    public void hideAll() {
+        jollyButton.setVisible(false);
+        vowelButton.setVisible(false);
+        turnButton.setVisible(false);
+        solutionButton.setVisible(false);
+        solutionTextField.setDisable(true);
+        letterTextField.setDisable(true);
+    }
+
+    public void hideButton() {
+        exitButton.setVisible(false);
+    }
 }
