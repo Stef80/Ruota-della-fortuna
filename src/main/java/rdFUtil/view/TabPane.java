36ddf7f59d3d57c29e15552eb2ab3cb569174b47
IvanDan <ivandanza@gmail.com>
2019-08-21 12:35:51 +0200

    recuperato TabPane

--- src/main/java/rdFUtil/view/TabPane.java
+++ src/main/java/rdFUtil/view/TabPane.java
@@ -1,4 +1,5 @@
 package rdFUtil.view;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,51 +8,46 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import rdFUtil.client.Client;
 import serverRdF.Server;
-import serverRdF.matchRdF.Match;
 import serverRdF.matchRdF.RemoteMatch;
 
 import java.net.URL;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-
-import java.rmi.registry.Registry;
-import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class TabPane implements Initializable {
 
-	@FXML
-	private ListView<RemoteMatch> gameList;
+    @FXML
+    private ListView<RemoteMatch> gameList;
 
-	private ObservableList<RemoteMatch> gameObservableList = FXCollections.observableArrayList();
+    private ObservableList<RemoteMatch> gameObservableList = FXCollections.observableArrayList();
+    private Client client;
+    private Server server;
+    private RemoteMatch match;
 
-	private Client client;
-	private Server server;
+    public TabPane(Server server, Client client) {
+        this.server = server;
+        this.client = client;
+    }
 
-	public TabPane(Server server){
-		this.server = server;
-	}
-	public void addMatch(ActionEvent actionEvent) throws RemoteException, NotBoundException {
-		/*RemoteMatch newMatch = (RemoteMatch) registry.lookup("newGame");
-		newMatch.addPlayer(client);
-		gameObservableList.add(newMatch);*/
-	}
+    public void addMatch(ActionEvent actionEvent) throws RemoteException, NotBoundException {
+//		match = new Match("id",LocalDateTime.now());//todo da aggiungere DBManager e EmailManager
+        gameObservableList.add(match);
+    }
 
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		gameList.setItems(gameObservableList);
-		gameList.setCellFactory(e -> new GameView());
-		/*
-		 * gameList.setCellFactory(new Callback<ListView<Game>, ListCell<Game>>(){
-		 *
-		 * @override
-		 * public ListCell<Game> call(ListView<Game> listGame){
-		 * return new GameView;
-		 * }
-		 * });
-		 * */
-	}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gameList.setItems(gameObservableList);
+        gameList.setCellFactory(e -> new GameView(server, client, match));
+
+//        gameList.setCellFactory(new Callback<ListView<Game>, ListCell<Game>>() {
+//            @Override
+//            public ListCell<Game> call(ListView<Game> listGame) {
+//                return new GameView;
+//            }
+//        });
+    }
 }
 
 
