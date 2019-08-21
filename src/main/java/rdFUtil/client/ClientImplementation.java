1450a53924b073c451975b439912c117ed45a274
IvanDan <ivandanza@gmail.com>
2019-08-21 12:33:20 +0200

    Update ClientImplementation.java

--- src/main/java/rdFUtil/client/ClientImplementation.java
+++ src/main/java/rdFUtil/client/ClientImplementation.java
@@ -57,55 +57,55 @@ public class ClientImplementation implements Client {
     public void notifyMancheResult(String winner) throws RemoteException {
     }
 
-	@Override
-	public void notifyNewManche(int numManche) throws RemoteException {
-	}
+    @Override
+    public void notifyNewManche(int numManche) throws RemoteException {
+    }
 
-	@Override
-	public void setNewPhrase(String theme, String phrase) throws RemoteException {
-	}
+    @Override
+    public void setNewPhrase(String theme, String phrase) throws RemoteException {
+    }
 
-	@Override
-	public void notifyNewTurn(String player) throws RemoteException {
-	}
+    @Override
+    public void notifyNewTurn(String player) throws RemoteException {
+    }
 
-	@Override
-	public void notifyYourTurn() throws RemoteException {
-	}
+    @Override
+    public void notifyYourTurn() throws RemoteException {
+    }
 
-	@Override
-	public void notifyEndMatch(String winner) throws RemoteException {
-	}
+    @Override
+    public void notifyEndMatch(String winner) throws RemoteException {
+    }
 
-	@Override
-	public void notifyMatchWin() throws RemoteException {
-	}
+    @Override
+    public void notifyMatchWin() throws RemoteException {
+    }
 
-	@Override
-	public void notifyPlayerStats(int position, String name, int partialPoints, int points) throws RemoteException {
-	}
+    @Override
+    public void notifyPlayerStats(int position, String name, int partialPoints, int points) throws RemoteException {
+    }
 
-	@Override
-	public void updatePhrase(boolean[] phrase) throws RemoteException {
-	}
+    @Override
+    public void updatePhrase(boolean[] phrase) throws RemoteException {
+    }
 
-	@Override
-	public void updatePhrase(char letter, int amount) throws RemoteException {
-	}
+    @Override
+    public void updatePhrase(char letter, int amount) throws RemoteException {
+    }
 
-	@Override
-	public void notifyTimeOut() throws RemoteException {
-	}
+    @Override
+    public void notifyTimeOut() throws RemoteException {
+    }
 
-	@Override
-	public void notifyWheelResult(String risultato) throws RemoteException {
-	}
+    @Override
+    public void notifyWheelResult(String risultato) throws RemoteException {
+    }
 
-	@Override
-	public void askForJolly() throws RemoteException {
-	}
+    @Override
+    public void askForJolly() throws RemoteException {
+    }
 
-	@Override
-	public void notifyPlayerError(String name) throws RemoteException {
-	}
+    @Override
+    public void notifyPlayerError(String name) throws RemoteException {
+    }
 }
