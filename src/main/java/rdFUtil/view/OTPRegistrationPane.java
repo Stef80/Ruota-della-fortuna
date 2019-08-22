b5124a5b1e021fd76374c92b95b80a4600b6065d
IvanDan <ivandanza@gmail.com>
2019-08-22 17:10:10 +0200

    update

--- src/main/java/rdFUtil/view/OTPRegistrationPane.java
+++ src/main/java/rdFUtil/view/OTPRegistrationPane.java
@@ -19,102 +19,98 @@ import org.controlsfx.control.Notifications;
 import rdFUtil.client.Client;
 import serverRdF.Server;
 import serverRdF.registrationRdF.OTPHelper;
+
 import java.io.IOException;
 import java.rmi.RemoteException;
 
-
 public class OTPRegistrationPane {
-	@FXML
+    @FXML
     private TextField otpTextField;
-	@FXML
-	private Button confirmButton;
-	@FXML
-	private Label timeLabel;
-	private int timeSeconds = 0 ;
-	private int timeMinutes = 10 ;
-	private Timeline timeline;
-	private Server server;
-	private Client client;
-	private OTPHelper otp;
+    @FXML
+    private Button confirmButton;
+    @FXML
+    private Label timeLabel;
+    private int timeSeconds = 0;
+    private int timeMinutes = 10;
+    private Timeline timeline;
+    private Server server;
+    private Client client;
+    private OTPHelper otp;
 
-	public OTPRegistrationPane(Server server, Client client,OTPHelper otp){
-		this.server = server;
-		this.client = client;
-		this.otp = otp;
-	}
+    public OTPRegistrationPane(Server server, Client client, OTPHelper otp) {
+        this.server = server;
+        this.client = client;
+        this.otp = otp;
+    }
 
-	public void enter() throws IOException {
-		String otpStr = otpTextField.getText();
-		boolean check = false;
-		try {
-			check =otp.checkOTP(otpStr,client);
-		} catch (RemoteException e) {
+    public void enter() throws IOException {
+        String otpStr = otpTextField.getText();
+        boolean check = false;
+        try {
+            check = otp.checkOTP(otpStr, client);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        if (!check) {
+            Notifications notification = Notifications.create()
+                    .title("OTP Notification")
+                    .text("CodiceOTP non valido\n")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.CENTER);
+            notification.showError();
+        } else {
+            timeline.stop();
+            Parent root = FXMLLoader.load(Thread.currentThread().getClass().getResource("main_pane.fxml"));
+            Scene scene = new Scene(root);
+            Stage primaryStage = new Stage();
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            primaryStage.setTitle("Wheel of Fortune");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            Stage thisStage = (Stage) confirmButton.getScene().getWindow();
+            thisStage.close();
+        }
+    }
 
-		}
-		if(!check){
-			Notifications notification = Notifications.create()
-												 .title("OTP Notification")
-												 .text("CodiceOTP non valido\n")
-												 .hideAfter(Duration.seconds(3))
-												 .position(Pos.CENTER);
-			notification.showError();
-		}else{
-			timeline.stop();
-			Parent root = FXMLLoader.load(Thread.currentThread().getClass().getResource("main_pane.fxml"));
-			Scene scene = new Scene(root);
-			Stage primaryStage = new Stage();
-			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-			primaryStage.setTitle("Wheel of Fortune");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			Stage thisStage = (Stage) confirmButton.getScene().getWindow();
-			thisStage.close();
-		}
-	}
+    public void runCountdown() {
+        timeLabel.setText(String.valueOf(timeMinutes) + ":" + String.valueOf(timeSeconds));
+        timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
 
-
-	public void runCountdown(){
-		timeLabel.setText(String.valueOf(timeMinutes)+":"+String.valueOf(timeSeconds));
-		timeline = new Timeline();
-		timeline.setCycleCount(Animation.INDEFINITE);
-		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-
-				if(timeSeconds == 0 ){
-					timeMinutes --;
-					timeSeconds = 59;
-				}
-				timeSeconds--;
-				timeLabel.setText(String.valueOf(timeMinutes)+":"+String.valueOf(timeSeconds));
-				if(timeSeconds<= 0 && timeMinutes <= 0) {
-					timeline.stop();
-					Notifications notification = Notifications.create()
-														 .title("OTP Notification")
-														 .text("Tempo esaurito \nripeti la procedura di registrazione")
-														 .hideAfter(Duration.seconds(3))
-														 .position(Pos.CENTER);
-					notification.showError();
-					Parent root = null;
-					try {
-						root = FXMLLoader.load(Thread.currentThread().getClass().getResource("main_pane.fxml"));
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					Scene scene = new Scene(root);
-					Stage primaryStage = new Stage();
-					scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-					primaryStage.setTitle("Wheel of Fortune");
-					primaryStage.setScene(scene);
-					primaryStage.show();
-					Stage thisStage = (Stage) confirmButton.getScene().getWindow();
-					thisStage.close();
-				}
-
-			}
-		}));
-		timeline.playFromStart();
-
-
-	}
+                if (timeSeconds == 0) {
+                    timeMinutes--;
+                    timeSeconds = 59;
+                }
+                timeSeconds--;
+                timeLabel.setText(String.valueOf(timeMinutes) + ":" + String.valueOf(timeSeconds));
+                if (timeSeconds <= 0 && timeMinutes <= 0) {
+                    timeline.stop();
+                    Notifications notification = Notifications.create()
+                            .title("OTP Notification")
+                            .text("Tempo esaurito \nripeti la procedura di registrazione")
+                            .hideAfter(Duration.seconds(3))
+                            .position(Pos.CENTER);
+                    notification.showError();
+                    Parent root = null;
+                    try {
+                        root = FXMLLoader.load(Thread.currentThread().getClass().getResource("main_pane.fxml"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Scene scene = new Scene(root);
+                    Stage primaryStage = new Stage();
+                    scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+                    primaryStage.setTitle("Wheel of Fortune");
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
+                    Stage thisStage = (Stage) confirmButton.getScene().getWindow();
+                    thisStage.close();
+                }
+            }
+        }));
+        timeline.playFromStart();
+    }
 }
