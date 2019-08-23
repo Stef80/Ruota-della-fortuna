8a3d9770ff87fb04e1d7f71010f4cc39c8f4f6ee
@Roto <rotoloni.gabriele@gmail.com>
2019-08-23 18:35:17 +0200

    +

---
+++ src/main/java/playerRdF/PrimePane.java
@@ -0,0 +1,29 @@
+package rdFUtil.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class PrimePane extends Application {
+    public static final boolean ISADMIN = false;
+    public PrimePane(){}
+
+    public static void main(String[] args) throws Exception {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Thread.currentThread().getContextClassLoader().getResource("wellcome_pane.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setTitle("Wheel of Fortune");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
