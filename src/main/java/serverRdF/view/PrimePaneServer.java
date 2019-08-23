26c0bf8cf0d8d15dc185768f45c02d6ce1931a7f
Stefano Longobucco <mide646@gmail.com>
2019-08-23 19:05:34 +0200

    madifiche server

---
+++ src/main/java/serverRdF/view/PrimePaneServer.java
@@ -0,0 +1,23 @@
+package serverRdF.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import serverRdF.dbComm.DBManager;
+
+public class PrimePaneServer extends Application {
+
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(Thread.currentThread().getContextClassLoader().getResource("server_main_pane.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		primaryStage.setTitle("Wheel of Fortune");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
