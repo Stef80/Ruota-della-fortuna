a89c4b0498e3f58913365c6a414485640b4e9af5
@Roto <rotoloni.gabriele@gmail.com>
2019-08-24 05:24:48 +0200

    Corretto il metodo per chi entra a meta'partita

--- src/main/java/rdFUtil/view/GamePlayerController.java
+++ src/main/java/rdFUtil/view/GamePlayerController.java
@@ -30,7 +30,7 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.StringTokenizer;
 
-public class GamePlayerController{
+public class GamePlayerController {
     @FXML
     private Button jollyButton;
     @FXML
@@ -92,42 +92,41 @@ public class GamePlayerController{
     private static RemoteMatch match;
     private Client client;
 
-    public GamePlayerController(){
+    public GamePlayerController() {
 
     }
 
-    public GamePlayerController( Client client){
+    public GamePlayerController(Client client) {
         this.client = client;
     }
 
-    public void createTableOfPhrase(){
+    public void createTableOfPhrase() {
 
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 14 ; j++) {
+            for (int j = 0; j < 14; j++) {
                 StackPane slotPane = new StackPane();
                 Label letterLabel = new Label();
                 letterLabel.setVisible(false);
                 slotPane.getChildren().add(letterLabel);
                 slotPane.setStyle(" -fx-background-color: #eefcf9;\n" +
-                                          "    -fx-border-color: #08FBE1;\n" +
-                                          "    -fx-border-radius: 3px;\n" +
-                                          "    -fx-background-radius: 3px;\n" +
-                                          "    -fx-border-width: 2px;");
-                phraseGridpane.add(slotPane,j,i);
+                        "    -fx-border-color: #08FBE1;\n" +
+                        "    -fx-border-radius: 3px;\n" +
+                        "    -fx-background-radius: 3px;\n" +
+                        "    -fx-border-width: 2px;");
+                phraseGridpane.add(slotPane, j, i);
             }
 
         }
-        setNewPhrase("ssx","Ciao, il mio nome è Michele Rovagnati!");
     }
 
-    public void updatePhrase(String letter){
+    public void updatePhrase(String letter) {
         StackPane node;
         Label label;
-        for(int i=0; i<5; i++){
-            for(int j=0; j<14; j++){
-                node = (StackPane)getNodeByRowColumnIndex(i,j);
-                label = (Label)node.getChildren().get(0);
-                if(label.getText().equals(letter)){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 14; j++) {
+                node = (StackPane) getNodeByRowColumnIndex(i, j);
+                label = (Label) node.getChildren().get(0);
+                if (label.getText().equals(letter)) {
                     label.setVisible(true);
                     node.setStyle(" -fx-background-color: #d6e2e0;\n" +
                             "    -fx-border-color: #08FBE1;\n" +
@@ -139,17 +138,17 @@ public class GamePlayerController{
         }
     }
 
-    public void updatePhrase(boolean[] phrase){
+    public void updatePhrase(boolean[] phrase) {
         int column = 0;
         int row = 0;
         int i = 0;
         StackPane node;
         Label label;
-        while(i<phrase.length){
-            node = (StackPane)getNodeByRowColumnIndex(row,column);
-            label = (Label)node.getChildren().get(0);
-            if(!label.isVisible()){
-                if(phrase[i] == true){
+        while (i < phrase.length) {
+            node = (StackPane) getNodeByRowColumnIndex(row, column);
+            label = (Label) node.getChildren().get(0);
+            if (!label.isVisible() && !label.getText().equals("")) {
+                if (phrase[i] == true) {
                     label.setVisible(true);
                     node.setStyle(" -fx-background-color: #d6e2e0;\n" +
                             "    -fx-border-color: #08FBE1;\n" +
@@ -157,89 +156,75 @@ public class GamePlayerController{
                             "    -fx-background-radius: 3px;\n" +
                             "    -fx-border-width: 2px;");
                     i++;
-                    if(column<13){
+                    if (column < 13) {
                         column++;
-                    }else{
+                    } else {
                         column = 0;
                         row++;
                     }
-                }else{
+                } else {
                     i++;
-                    if(column<13){
+                    if (column < 13) {
                         column++;
-                    }else{
+                    } else {
                         column = 0;
                         row++;
                     }
                 }
-            }else {
-                if(node.getStyle().equals(" -fx-background-color: #d6e2e0;\n" +
-                        "    -fx-border-color: #08FBE1;\n" +
-                        "    -fx-border-radius: 3px;\n" +
-                        "    -fx-background-radius: 3px;\n" +
-                        "    -fx-border-width: 2px;")){
-                    i++;
-                    if(column<13){
-                        column++;
-                    }else{
-                        column = 0;
-                        row++;
-                    }
-                }else{
-                    if(column<13){
-                        column++;
-                    }else{
-                        column = 0;
-                        row++;
-                    }
+            } else {
+                if (column < 13) {
+                    column++;
+                } else {
+                    column = 0;
+                    row++;
                 }
             }
         }
     }
 
-    public void setNewPhrase(String theme, String phrase){
+    public void setNewPhrase(String theme, String phrase) {
         phraseThemeLabel.setText(theme);
         int column = 0;
         int row = 0;
         char car;
         StackPane node;
         phrase = phrase.toUpperCase();
-        StringTokenizer st = new StringTokenizer(phrase," ',!?.:;\"/()\\^<>-+*");
+        StringTokenizer st = new StringTokenizer(phrase, " ',!?.:;\"/()\\^<>-+*");
         String s = st.nextToken();
         Label label;
         int pointer = 0;
-        for(int i=0; i<phrase.length(); i++){
+        for (int i = 0; i < phrase.length(); i++) {
             car = phrase.charAt(i);
-            if(car != ' ' && car != ',' && car != '\'' && car != '.' && car != ':' && car != ';' && car != '?' && car != '!' && car != '"' && car != '/' && car != '\\' && car != '(' && car != ')'
-                    && car != '^' && car != '<' && car != '>' && car != '-' && car != '+' && car != '*'){
-                if(pointer == s.length()-1){
-                    if(st.hasMoreTokens()) {
+            if (car != ' ' && car != ',' && car != '\'' && car != '.' && car != ':' && car != ';' && car != '?' && car != '!' && car != '"' && car != '/' && car != '\\' && car != '(' && car != ')'
+                    && car != '^' && car != '<' && car != '>' && car != '-' && car != '+' && car != '*') {
+                if (pointer == s.length() - 1) {
+                    if (st.hasMoreTokens()) {
                         s = st.nextToken();
                         pointer = -1;
                     }
-                }else if(pointer == 0){
-                    if(s.length() > (14-column)){
+                } else if (pointer == 0) {
+                    if (s.length() > (14 - column)) {
                         column = 0;
                         row++;
                     }
                 }
-                node = (StackPane)getNodeByRowColumnIndex(row,column);
-                label = (Label)node.getChildren().get(0);
-                label.setText(""+car);
+                node = (StackPane) getNodeByRowColumnIndex(row, column);
+                label = (Label) node.getChildren().get(0);
+                label.setText("" + car);
                 node.setStyle(" -fx-background-color: #bbbebd;\n" +
                         "    -fx-border-color: #08FBE1;\n" +
                         "    -fx-border-radius: 3px;\n" +
                         "    -fx-background-radius: 3px;\n" +
                         "    -fx-border-width: 2px;");
                 pointer++;
-                if(column<13){
+                if (column < 13) {
                     column++;
-                }else{
+                } else {
                     column = 0;
                     row++;
                 }
-            }else{
-                if(!(car == ' ' && column == 0)) {
+            } else {
+                if (!(car == ' ' && column == 0)) {
                     node = (StackPane) getNodeByRowColumnIndex(row, column);
                     label = (Label) node.getChildren().get(0);
                     label.setText("" + car);
@@ -252,19 +237,19 @@ public class GamePlayerController{
                         column = 0;
                         row++;
                     }
-                }else{
+                } else {
 
                 }
             }
         }
     }
 
-    private Node getNodeByRowColumnIndex (final int row, final int column) {
+    private Node getNodeByRowColumnIndex(final int row, final int column) {
         Node result = null;
         ObservableList<Node> childrens = phraseGridpane.getChildren();
 
         for (Node node : childrens) {
-            if(phraseGridpane.getRowIndex(node) == row && phraseGridpane.getColumnIndex(node) == column) {
+            if (phraseGridpane.getRowIndex(node) == row && phraseGridpane.getColumnIndex(node) == column) {
                 result = node;
                 break;
             }
@@ -282,7 +267,8 @@ public class GamePlayerController{
         solutionTextField.setDisable(true);
         letterTextfield.setDisable(true);
     }
-    public void disableAll(){
+
+    public void disableAll() {
         jollyButton.setDisable(true);
         vowelButton.setDisable(true);
         spinButton.setDisable(true);
@@ -291,7 +277,7 @@ public class GamePlayerController{
         letterTextfield.setDisable(true);
     }
 
-    public void activeAll(){
+    public void activeAll() {
         jollyButton.setDisable(false);
         vowelButton.setDisable(false);
         spinButton.setDisable(false);
@@ -306,7 +292,7 @@ public class GamePlayerController{
         timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                 timeSeconds-- ;
+                timeSeconds--;
                 timerLabel.setText(String.valueOf(timeSeconds));
                 if (timeSeconds <= 0)
                     timeline.stop();
@@ -321,70 +307,71 @@ public class GamePlayerController{
         solutionTextField.setDisable(false);
         runCountdown(10);
     }
+
     @FXML
     public void confirmSolution() throws RemoteException {
-        String solution =solutionTextField.getText();
+        String solution = solutionTextField.getText();
         match.giveSolution(solution);
     }
 
-    public void wheelSpin(){
+    public void wheelSpin() {
         try {
-         wheelResult = match.wheelSpin();
+            wheelResult = match.wheelSpin();
         } catch (RemoteException e) {
             e.printStackTrace();
         }
         if (wheelResult != 0) runCountdown(5);
     }
 
-    public void wheelResult(String result){
+    public void wheelResult(String result) {
         resultLabel.setText(result);
     }
-     @FXML
-     public void onEnter() throws RemoteException {
-        String letter =  letterTextfield.getText();
-     if(spinButton.isPressed()){
-           match.giveConsonant(letter,wheelResult);
-        }else if(vowelButton.isPressed()){
+
+    @FXML
+    public void onEnter() throws RemoteException {
+        String letter = letterTextfield.getText();
+        if (spinButton.isPressed()) {
+            match.giveConsonant(letter, wheelResult);
+        } else if (vowelButton.isPressed()) {
             match.giveVocal(letter);
         }
-     }
-     public void giveVocal() throws RemoteException {
+    }
+
+    public void giveVocal() throws RemoteException {
         match.askForVocal();
         runCountdown(5);
-     }
+    }
 
-     public void giveJolly() throws RemoteException {
+    public void giveJolly() throws RemoteException {
         match.jolly();
-     }
+    }
 
     public void setTurn(String nickName) throws RemoteException {
-        if(nickName.equals(player1Label.getText())){
-           player1Box.setStyle("");
-           player2Box.setStyle("");
-           player3Box.setStyle("");
-        }else
-            if(nickName.equals(player2Label.getText())){
+        if (nickName.equals(player1Label.getText())) {
             player1Box.setStyle("");
             player2Box.setStyle("");
             player3Box.setStyle("");
-        }else
-            if(nickName.equals(player3Label.getText())){
+        } else if (nickName.equals(player2Label.getText())) {
+            player1Box.setStyle("");
+            player2Box.setStyle("");
+            player3Box.setStyle("");
+        } else if (nickName.equals(player3Label.getText())) {
             player1Box.setStyle("");
             player2Box.setStyle("");
             player3Box.setStyle("");
         }
-         if(!nickName.equals(client.getNickname())){
-           disableAll();
-         }
-         runCountdown(5);
+        if (!nickName.equals(client.getNickname())) {
+            disableAll();
+        }
+        runCountdown(5);
     }
 
-    public void yourTurn(){
+    public void yourTurn() {
         activeAll();
     }
 
-    public void notifyPlayerStats(int pos, String nickname, int partial, int total, int numJolly){
-        switch(pos){
+    public void notifyPlayerStats(int pos, String nickname, int partial, int total, int numJolly) {
+        switch (pos) {
             case 0:
                 player1Label.setText(nickname);
                 partial1Label.setText(String.valueOf(partial));
@@ -406,66 +393,67 @@ public class GamePlayerController{
         }
     }
 
-      public void vocalcallnotify(String nickname){
-        String message = nickname+ " ha chiamato la vocale";
-          Notifications notification = Notifications.create()
-                                               .title("Mosse")
-                                               .text(message)
-                                               .hideAfter(Duration.seconds(2))
-                                               .position(Pos.BASELINE_CENTER);
-          notification.showInformation();
-      }
-
-      public void callSolutionNotify(String nickname){
-        String message = nickname + " da la soluzione ";
-          Notifications notification = Notifications.create()
-                                               .title("Mosse")
-                                               .text(message)
-                                               .hideAfter(Duration.seconds(2))
-                                               .position(Pos.BASELINE_CENTER);
-          notification.showInformation();
-
-      }
-      public void jollyNotify(String nickname){
-          String message = nickname + " ha giocato il jolly";
-          Notifications notification = Notifications.create()
-                                               .title("Mosse")
-                                               .text(message)
-                                               .hideAfter(Duration.seconds(2))
-                                               .position(Pos.BASELINE_CENTER);
-          notification.showInformation();
-
-      }
-
-    public void callLetterNotify(String nickname , String letter){
-        String message = nickname + " ha scelto la lettera " + letter;
+    public void vocalcallnotify(String nickname) {
+        String message = nickname + " ha chiamato la vocale";
         Notifications notification = Notifications.create()
-                                             .title("Mosse")
-                                             .text(message)
-                                             .hideAfter(Duration.seconds(2))
-                                             .position(Pos.BASELINE_CENTER);
+                .title("Mosse")
+                .text(message)
+                .hideAfter(Duration.seconds(2))
+                .position(Pos.BASELINE_CENTER);
+        notification.showInformation();
+    }
+
+    public void callSolutionNotify(String nickname) {
+        String message = nickname + " da la soluzione ";
+        Notifications notification = Notifications.create()
+                .title("Mosse")
+                .text(message)
+                .hideAfter(Duration.seconds(2))
+                .position(Pos.BASELINE_CENTER);
         notification.showInformation();
 
     }
 
-    public static void setMatch(RemoteMatch matc){
-    	match = matc;
-	}
+    public void jollyNotify(String nickname) {
+        String message = nickname + " ha giocato il jolly";
+        Notifications notification = Notifications.create()
+                .title("Mosse")
+                .text(message)
+                .hideAfter(Duration.seconds(2))
+                .position(Pos.BASELINE_CENTER);
+        notification.showInformation();
 
-	public static void setObserver(boolean observer){
+    }
+
+    public void callLetterNotify(String nickname, String letter) {
+        String message = nickname + " ha scelto la lettera " + letter;
+        Notifications notification = Notifications.create()
+                .title("Mosse")
+                .text(message)
+                .hideAfter(Duration.seconds(2))
+                .position(Pos.BASELINE_CENTER);
+        notification.showInformation();
+
+    }
+
+    public static void setMatch(RemoteMatch matc) {
+        match = matc;
+    }
+
+    public static void setObserver(boolean observer) {
         isObserver = observer;
-	}
+    }
 
-	public void exitMatch(ActionEvent actionEvent) throws IOException {
+    public void exitMatch(ActionEvent actionEvent) throws IOException {
         try {
             if (isObserver) {
                 match.leaveMatchAsObserver(client);
-            }else{
+            } else {
                 match.leaveMatchAsPlayer(client);
             }
-        }catch (RemoteException e){
+        } catch (RemoteException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             match = null;
             Parent root = FXMLLoader.load(Thread.currentThread().getContextClassLoader().getResource("tab_pane.fxml"));
             Stage primaryStage = new Stage();
