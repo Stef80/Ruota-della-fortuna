5b340f13b4dd10965b59a7e6e9a90e8e8bb6654e
@Roto <rotoloni.gabriele@gmail.com>
2019-08-24 18:02:16 +0200

    +

--- src/main/java/rdFUtil/view/ForgottenPasswordPane.java
+++ src/main/java/rdFUtil/view/ForgottenPasswordPane.java
@@ -1,12 +1,16 @@
 package rdFUtil.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import rdFUtil.client.Client;
 import serverRdF.Server;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 
 public class ForgottenPasswordPane {
@@ -14,6 +18,8 @@ public class ForgottenPasswordPane {
 	private Button enterButton;
 	@FXML
 	private TextField mailText;
+	@FXML
+	private Button backButton;
 	private Server server;
 	private Client client;
 
@@ -30,5 +36,17 @@ public class ForgottenPasswordPane {
 		server.resetPassword(client, mail);
 		Stage oldStage = (Stage) enterButton.getScene().getWindow();
 		oldStage.close();
+	}
+
+	public void back() throws IOException {
+		Parent root = FXMLLoader.load(Thread.currentThread().getClass().getResource("OTP_registration_pane.fxml"));
+		Scene scene = new Scene(root);
+		Stage primaryStage = new Stage();
+		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		primaryStage.setTitle("Wheel of Fortune");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		Stage thisStage = (Stage) backButton.getScene().getWindow();
+		thisStage.close();
 	}
 }
