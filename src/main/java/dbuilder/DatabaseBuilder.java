8e4dc94643255b6882b71062ec105dbd6a51054a
IvanDan <ivandanza@gmail.com>
2019-08-27 12:30:45 +0200

    update

--- src/main/java/dbuilder/DatabaseBuilder.java
+++ src/main/java/dbuilder/DatabaseBuilder.java
@@ -1,10 +1,13 @@
 package dbuilder;
 
 import rdFUtil.database.DatabaseHelper;
+import serverRdF.dbComm.DBManager;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -18,17 +21,19 @@ public class DatabaseBuilder {
     private static final String PATH = "src/main/java/dbuilder/database_creator.sql";
     private static Scanner scan;
     private static Statement statement;
-
+    private static DBManager dbManager;
     public static void main(String[] args) throws SQLException, IOException {
-//        try(Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "postgres")){
-//            statement=connection.createStatement();
-//        }catch (SQLException ex){
-//            ex.printStackTrace();
-//        }
-//        statement.executeQuery();
+        try(Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "postgres")){
+            statement=connection.createStatement();
+            System.out.println("ok");
+        }catch (SQLException ex){
+            System.out.println("no");
+            ex.printStackTrace();
+        }
 
         File cazzoDiFileCheNonSiLegge = new File(PATH);
-        db = new DatabaseHelper("localhost", 5433, "postgres", "postgres", "postgres");
+//        db = new DatabaseHelper("localhost", 5433, "postgres", "postgres", "postgres");
+//        dbManager=DBManager.createDBManager("jdbc:postgresql://localhost:5433/postgres","postgres","postgres");
         sb = new StringBuilder("");
         scan=new Scanner(cazzoDiFileCheNonSiLegge);
         while (scan.hasNextLine()) {
@@ -37,7 +42,6 @@ public class DatabaseBuilder {
         }
         System.out.println(sb.toString());
         String s=sb.toString();
-        db.queryDB(s);
-        db.close();
+        statement.executeUpdate(s);
     }
 }
