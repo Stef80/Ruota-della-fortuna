c9994ed91ae4519b4c7a99d5a289b6ef1f075f9a
IvanDan <ivandanza@gmail.com>
2019-08-27 18:10:30 +0200

    Update DatabaseBuilder.java

--- src/main/java/dbuilder/DatabaseBuilder.java
+++ src/main/java/dbuilder/DatabaseBuilder.java
@@ -5,7 +5,6 @@ import serverRdF.dbComm.DBManager;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,24 +18,25 @@ public class DatabaseBuilder {
     private static String url;
     private static BufferedReader br;
     private static StringBuilder sb;
-    private static final String PATH = "E:\\git\\ruota-della-fortuna\\src\\main\\java\\dbuilder\\database_creator.sql";
+    private static final String PATH = "src/main/java/dbuilder/database_creator.sql";
     private static Scanner scan;
     private static Statement statement;
     private static DBManager dbManager;
-    public static void main(String[] args) throws SQLException, IOException {
-        try(Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "paco4ever")){
-            statement=connection.createStatement();
+
+    public static void main(String[] args) throws IOException {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "postgres")) {
+            statement = connection.createStatement();
             System.out.println("ok");
             File cazzoDiFileCheNonSiLegge = new File(PATH);
             sb = new StringBuilder("");
-            scan=new Scanner(cazzoDiFileCheNonSiLegge);
+            scan = new Scanner(cazzoDiFileCheNonSiLegge);
             while (scan.hasNextLine()) {
                 sb.append(scan.nextLine());
             }
             System.out.println(sb.toString());
-            String s=sb.toString();
+            String s = sb.toString();
             statement.executeUpdate(s);
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("no");
             ex.printStackTrace();
         }
